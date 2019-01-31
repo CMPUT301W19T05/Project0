7ddccb108e0b4104c4076fb2b33a188557aa46a0
samlhl <samlhl9343@gmail.com>
2019-01-30 22:02:12 -0700

    Update Shape.java

--- app/src/main/java/lab4/project0/Shape.java
+++ app/src/main/java/lab4/project0/Shape.java
@@ -3,6 +3,7 @@ package lab4.project0;
 public abstract class Shape {
     private int x;
     private int y;
-    private int z;
+    private int z3;
+    private int z4;
     private String[] color;
 }
